16b8d92085bf130681388f8a9f4d634107924fcd
ivocat <alyrn.n@gmail.com>
2018-05-15 17:22:17 +0300

    Implemented first task requirements

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Testguru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.I18n.available_locales = [:en, :ru]
+    config.I18n.default_locale = :ru
+    config.I18n.fallbacks = [:en]
+
+    config.time_zone = 'Moscow'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
