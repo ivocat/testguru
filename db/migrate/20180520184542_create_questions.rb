ac82d4454fc840919e32159f4e2d5469503c6aa9
ivocat <alyrn.n@gmail.com>
2018-05-20 21:46:18 +0300

    Added models for basic entities

---
+++ db/migrate/20180520184542_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.text :body
+      t.integer :test_id
+
+      t.timestamps
+    end
+  end
+end
