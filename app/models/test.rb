0217095b1bea3ebbf7d6c7b114eae4d7d3485b1d
ivocat <alyrn.n@gmail.com>
2018-05-28 22:01:12 +0300

    Fixed by category search for tests list

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.list_tests_by_category(category_name)
-    tests.where(category: category_name).order(title: :desc)
+    tests.where(category_id: categories.find_by(name: category_name).id).order(title: :desc)
   end
 end
