0a777957671beaaa6a088dad4b6275c440c916db
ivocat <alyrn.n@gmail.com>
2018-07-05 20:33:17 +0300

    Added uniqueness index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_100751) do
+ActiveRecord::Schema.define(version: 2018_07_05_173112) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_07_01_100751) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
